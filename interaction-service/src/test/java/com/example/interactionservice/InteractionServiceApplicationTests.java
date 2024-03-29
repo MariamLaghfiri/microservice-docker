28b3dc3b4c53e50db2d6cf655ab3c94f731fd67c
mariamL <laghfirimariam@gmail.com>
2024-03-29 15:16:08 +0000

    add documentation

--- interaction-service/src/test/java/com/example/interactionservice/InteractionServiceApplicationTests.java
+++ interaction-service/src/test/java/com/example/interactionservice/InteractionServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.interactionservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class InteractionServiceApplicationTests {
-
-//	@Test
-//	void contextLoads() {
-//	}
-
-}
+//package com.example.interactionservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class InteractionServiceApplicationTests {
+//
+////	@Test
+////	void contextLoads() {
+////	}
+//
+//}
